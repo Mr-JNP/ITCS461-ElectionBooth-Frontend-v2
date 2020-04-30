def554ac0a38230786bbd452629113990e512c25
Nontapat Pintira <nontapat.pin@gmail.com>
2020-04-30 15:41:12 +0700

    Changes API_BASE_URL

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://localhost:8443/api';
 //export const API_BASE_URL = '/api';
 export const ACCESS_TOKEN = 'accessToken';
 
